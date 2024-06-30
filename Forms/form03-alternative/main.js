8fafdb20770ecad9d384e51390196a2730008a71
SaiSayNoomLeng-haru <saisaynoomleng.haru.9723@gmail.com>
2024-06-29 21:53:04 -0500

    form03 alternative finished

---
+++ Forms/form03-alternative/main.js
@@ -0,0 +1,41 @@
+$(document).ready(()=>{
+    const container1 = $('#container1');
+    const container2 = $('#container2');
+
+    const signUp = $('#sign-up-btn');
+    const signIn = $('#sign-in-btn');
+
+    const left1 = $('#left1');
+    const left2 = $('#left2');
+
+    const right1 = $('#right1');
+    const right2 = $('#right2');
+
+  
+    signUp.click(()=>{
+        left1.addClass('animated-left');
+        right1.addClass('animated-right');
+        
+        setTimeout(()=>{
+            container1.addClass('hide');
+            container2.removeClass('hide');
+            left1.removeClass('animated-left');
+            right1.removeClass('animated-right');
+        },1000);
+      
+    });
+
+    signIn.click(()=>{
+        left2.addClass('animated-left');
+        right2.addClass('animated-right');
+
+        setTimeout(()=>{
+            container2.addClass('hide');
+            container1.removeClass('hide');
+            left2.removeClass('animated-left');
+            right2.removeClass('animated-right');
+        },1000)
+    });
+
+
+});
